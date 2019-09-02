bc23ed8cfc891ef02f07abf870b6ae93009abd6b
Philip <philip.grach@gmail.com>
2019-09-02 17:01:14 +0300

    Task 1
    1) I turned the api to a router and and moved the functionality to a post end point. If using router was not intended it's possible to achieve the same result by checking if (req.method == 'POST') and sending a 405
    
    2) I check with countDocuments instead of find since it's supposed to be faster.
    
    * Made create into a router
    * Added a check for same email

--- api/account/create.js
+++ api/account/create.js
@@ -1,8 +1,22 @@
+const express = require('express');
+const router = express.Router();
 const Account = require('../../models/account/Account');
 
-module.exports = async function(req, res, next) {
-	const {email, name, age} = req.body;
-	const account = new Account({email, name, age});
-	await account.save();
-	return res.send({message: 'success'});
-};
+router.post('/', async (req, res, send) => {
+
+    const { email, name, age } = req.body;
+
+    // Checks if email already exists
+    await Account.countDocuments({ email }, async (err, count) => {
+        if (err)
+            return res.status(500).send({ error: 'database error' });
+        if (count > 0)
+            return res.status(422).send({ error: 'email already exists' });
+
+        const account = new Account({ email, name, age });
+        await account.save();
+        return res.status(201).send({ message: 'success' });
+    });
+});
+
+module.exports = router;
